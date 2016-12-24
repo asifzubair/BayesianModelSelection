5c64c81c00a317b755d70ac0ffd01406df9a43c6
Asif Zubair <asif.zubair@gmail.com>
2016-12-23 20:52:54 -0800

    Update mcmc_utils.py

--- mcmc_utils.py
+++ mcmc_utils.py
@@ -1,20 +1,29 @@
-import numpy as np
-from math import log, pi
+from math import log
+from math import pi
+from scipy.stats import uniform
 from scipy.stats import multivariate_normal
 from pymc.Matplot import plot
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def initialise(prior_min, prior_max):
+    init = [uniform.rvs(lower, upper-lower) for lower,upper in zip(prior_min,prior_max)]
+    init[3] = int(init[3])
+    return init
 
 def proposal_step(theta, initial_variance):
-    if np.uniform.rvs() > 0.9:
-        t =  theta.copy()
-        t[-1] = t[-1] + np.random.choice([-1,1],1)[0]
+    t =  theta.copy()
+    if uniform.rvs() > 0.9:
+        t[3] = t[3] + np.random.choice([-1.,1.],1)[0]
         return t
-    return multivariate_normal.rvs(theta, initial_variance, 1)
+    new_theta = multivariate_normal.rvs(np.append(theta[:3], theta[4:]), initial_variance, 1)
+    new_theta = np.append(np.append(new_theta[:3], t[3]), new_theta[3:])
+    return new_theta
 
 def reflect_proposal(parametersProposed, parametersLower, parametersUpper):
     """reflectProposal: We use reflecting boundaries"""
+
     for ii in range(np.size(parametersProposed)):
         while (parametersProposed[ii] < parametersLower[ii] or parametersUpper[ii] < parametersProposed[ii]):
             if(parametersProposed[ii] < parametersLower[ii]):
@@ -25,12 +34,14 @@ def reflect_proposal(parametersProposed, parametersLower, parametersUpper):
 
 def calculate_log_likelihood(target_data, exp_error, weight_vector, params, model):
     """Log likelihood of the data given the parameters"""
+
     exact_solution = model(params)
     LL = -(0.5)*(log(2*pi) + 2*log(exp_error) + ((exact_solution - target_data)**2)/(exp_error**2))
     return weight_vector.dot(LL)
 
 def exchange(chain, num_chains):
     """Exchange scheme"""
+
     if not chain:
         return 1
     if chain == num_chains-1:
@@ -43,6 +54,7 @@ def sub_sample(df, burn_in = 5000, thin = 10):
 
 def trace_plot(file_name, suffix='', burn_in=5000, thin=10, likelihood_only=False, verbose=1, last=False):
     """Plots traces of various kinds"""
+
     df = pd.read_table(file_name)
     if verbose:
         print "*" * 40
@@ -62,6 +74,7 @@ def trace_plot(file_name, suffix='', burn_in=5000, thin=10, likelihood_only=Fals
 
 def marginal_likelihood(likelihood, temps):
     """Gives the estimate for the marginal likelihood"""
+    
     delta = np.diff(temps)    
     delta = np.append(np.append(0,delta),0)
     deltas = delta[:-1] + delta[1:]
@@ -81,15 +94,4 @@ def posterior_sample(y, df, predict, name="sample_posteriors", sample_every=10):
             a.plot(Ji[i*100:100+i*100],'b-')
             a.set_title(gap[i])
     plt.show()
-
-def plot_expression(y, predict, parms, name="expression profile"):
-    gap = ["Hb","Kni","Kr","Gt"]
-    fig, ax = plt.subplots(2,2)
-    for i, a in enumerate(ax.flatten()):
-        a.plot(y[i*100:100+i*100],"black")
-        a.set_title(gap[i])
-    Ji = predict(parms)
-    for i, a in enumerate(ax.flatten()):
-        a.plot(Ji[i*100:100+i*100],'b-')
-        a.set_title(gap[i])
-    plt.show()
+    fig.savefig(name, format='pdf', dpi=1200)
