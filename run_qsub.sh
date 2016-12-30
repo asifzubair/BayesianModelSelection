821cff50d8a2865e0d529ce5b0303e3eea280e25
asifzubair <asif.zubair@gmail.com>
2016-12-30 15:25:59 -0800

    stuff

--- run_qsub.sh
+++ run_qsub.sh
@@ -106,3 +106,29 @@ for i in `echo {01..10}`; do
 	qsubHead.sh -s run_mc8_${i} -w 500 -m 5gb
 	echo python mcmc.py configs/config_c8/config_mc8_${i}.yml >> run_mc8_${i}.qsub
 done
+
+for i in `echo {01..10}`; do
+echo "mcmc:
+    seed: `echo $RANDOM`
+    num_steps: 1000000
+    num_chains: 10
+    num_pairs: 10
+    error: 0.1
+    do_exchange: True
+    dump_all: True
+    suffix: pt_1M
+    dir_name: m_B_Kr7r/run_${i}
+    initial_variance: [0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001]
+model:
+    class_file: models
+    object: models.PapaModel_B_Kr7r()
+    param_names: [alpha, D, Co, Ns, K, K1, K3]
+    initial_values: 
+    prior_min: [0.0001, 0.0001, 0.0001, 1., 3.5, 3.5, 3.5]
+    prior_max: [1., 2., 2., 200., 9., 9., 9.]" > configs/config_B_Kr7r/config_B_Kr7r_${i}.yml
+done
+
+for i in `echo {01..10}`; do
+    qsubHead.sh -s run_mB_Kr7r_${i} -w 500 -m 5gb
+    echo python mcmc.py configs/config_B_Kr7r/config_B_Kr7r_${i}.yml >> run_mB_Kr7r_${i}.qsub
+done
