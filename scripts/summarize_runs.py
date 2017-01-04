cea0642f3854ecd9e760acd1a8f939d69562e588
Asif Zubair <asif.zubair@gmail.com>
2017-01-03 21:51:00 -0800

    Create summarize_runs.py

---
+++ scripts/summarize_runs.py
@@ -0,0 +1,15 @@
+from math import log
+from math import pi
+from scipy.stats import uniform
+from scipy.stats import multivariate_normal
+from pymc.Matplot import plot
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+from utils import *
+sys.path.append("..")
+import models
+
+temps = 
