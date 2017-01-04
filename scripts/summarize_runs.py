b0b9c78afd403694e2bd380646a0e85cba7d74e5
Asif Zubair <asif.zubair@gmail.com>
2017-01-03 23:19:39 -0800

    Update summarize_runs.py

--- scripts/summarize_runs.py
+++ scripts/summarize_runs.py
@@ -1,11 +1,5 @@
-from math import log
-from math import pi
-from scipy.stats import uniform
-from scipy.stats import multivariate_normal
-from pymc.Matplot import plot
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import sys, os
 
 from utils import *
@@ -16,31 +10,33 @@ num_chains = 10
 temps = np.array([1.*(i+1)/(num_chains) for i in range(num_chains)])**5
 
 model = sys.argv[1]
-runs = sys.argv[2]
+runs_dir = sys.argv[2]
 
 BURNIN = 300000
 THIN = 100
 
 if model == "ma6":
-  m = PapaModelA6()
+    m = PapaModelA6()
 elif model == "mb7":
-  m = PapaModelB7()
+    m = PapaModelB7()
 elif model == "mb7r":
-  m = PapaModelB7r()
+    m = PapaModelB7r()
 elif model == "mc8":
-  m = PapaModelC8()
+    m = PapaModelC8()
 elif model == "mB_Kr7":
-  m = PapaModel_B_Kr7()
+    m = PapaModel_B_Kr7()
 elif model == "mB_Kr7r":
-  m = PapaModel_B_Kr7r()
+    m = PapaModel_B_Kr7r()
 elif model == "mB_Kr8":
-  m = PapaModel_B_Kr8()
+    m = PapaModel_B_Kr8()
 
 predict = m.predict()
 data = m.y
 
+runs = ["run_" + str(i).zfill(2) for i in range(1, 11)]
+runs = [os.path.join(runs_dir, r) for r in runs]
 
-df = trace_plot(os.path.join("run_1", "posteriors_pt_1M_10.csv"), burn_in = BURNIN, thin = THIN, name = )
-df1 = sub_sample(pd.read_table(""), burn_in = BURNIN, thin = THIN)
-
-
+for dir in runs:
+    post = [os.path.join(dir, "posteriors_pt_1M_" + str(i) + ".csv") for i in range(1, 11)]
+    likelihoods = np.array([np.array(open_run_subsample(file, BURNIN, THIN)["LnLike"]) for file in post])
+    print marginal_likelihood(likelihoods, temps)
